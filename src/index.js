8fe784cbde895b745a2f720dff1ce582b80e47e6
Naushad Alam <64242470+naushad2007@users.noreply.github.com>
2021-11-22 07:12:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Footer from "./components/Footer" ;
 
 ReactDOM.render(
   <React.StrictMode>
