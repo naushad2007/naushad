3fd46a1d9badd480213322cb17acf3cbc2aa36d0
Naushad Alam <64242470+naushad2007@users.noreply.github.com>
2021-11-22 07:16:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,32 @@ import Footer from "./components/Footer" ;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+      <Footer />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
