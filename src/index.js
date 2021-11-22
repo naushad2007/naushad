a0959aef0c3ca4f6029d2cec4429aa3e26670e67
Naushad Alam <64242470+naushad2007@users.noreply.github.com>
2021-11-22 07:21:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Footer from "./components/Footer" ;
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
