6c4f0bc323928c7f15669d5182b3b378db332fd3
Naushad Alam <64242470+naushad2007@users.noreply.github.com>
2021-11-22 07:10:33 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div style={{width:"100%",backgroundColor:"black",color:"#fff",height:"10vh",display:"flex",justifyContent:"center"}}>
+            <p style={{textAlign:"center"}}>  copyright 2021 All Right Reserved || textutilities-msn3p.ondigitalocean.app </p>
+        </div>
+    )
+}
+
+export default Footer
